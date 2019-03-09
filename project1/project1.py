3e13c9d72deade99205f97050837a9e2f001e054
Lana Gorlinski <lanagor@Lanas-MacBook-Air-2.local>
2019-03-08 20:34:52 -0500

    got extinction working

--- project1/project1.py
+++ project1/project1.py
@@ -14,21 +14,28 @@ def hyperbolic(R, ITI, x):
 def plot_exponentials(D1, Rs, gammas, maxY=None):
     maxY = max(D1) if maxY is None else maxY
     for R_ratio, gamma in itertools.product(Rs, gammas):
-        label = f'E: g={gamma}, r={R_ratio:.2f}'
+        # label = f'E: g={gamma}, r={R_ratio:.2f}'
         D2 = np.log(R_ratio) / np.log(gamma) + D1
         D1 = D1[np.nonzero(D2 < maxY)]
         D2 = D2[np.nonzero(D2 < maxY)]
+        # plt.plot(D1, D2, label=label)
+        label = "E: g={}, r={}".format(gamma, R_ratio)
         plt.plot(D1, D2, label=label)
-
+        # plt.legend(loc='best')
+        # plt.show()
 
 def plot_hyperbolics(D1, Rs, ITIs, maxY=None):
     maxY = max(D1) if maxY is None else maxY
     for R_ratio, ITI in itertools.product(Rs, ITIs):
-        label = f'H: ITI={ITI}, r={R_ratio:.2f}'
+        # label = f'H: ITI={ITI}, r={R_ratio:.2f}'
         D2 = (ITI + D1) / R_ratio
         D1 = D1[np.nonzero(D2 < maxY)]
         D2 = D2[np.nonzero(D2 < maxY)]
-        plt.plot(D1, D2, label=label)
+        # plt.plot(D1, D2, label=label)
+        label = "H: ITI={}, r={}".format(ITI, R_ratio)
+        plt.plot(D1, D2,label=label)
+        # plt.legend(loc='best')
+        # plt.show()
 
 D = np.linspace(20, 100, 1000)
 plot_exponentials(D, [64, 1 / 64], [2, 4])
